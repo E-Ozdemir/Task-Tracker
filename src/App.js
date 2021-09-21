36892e24fc50cf49b29c53c77cbf4a28366817aa
E-Ozdemir <emreclarusway@gmail.com>
2021-09-21 16:23:43 +0200

    delete function added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+
 import {useState} from 'react'
 import { initialState } from './store/initialState';
+import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState(initialState);
+
+  //create task
+
+  //delete task
+  const onDelete = (deletedItemID)=> setTasks(tasks.filter((task)=>task.id !== deletedItemID));
+
+
+
+  //toggle done
+
+  //toggle show and hide
   return (
     <div className="container">
       <Header title="Task Tracker"/>
-      <Tasks tasks={tasks}/>
+      <TaskList tasks={tasks} onDelete={onDelete}/>
     </div>
   );
 }
---
+++ src/components/TaskList.js
@@ -0,0 +1,17 @@
+
+import { TaskItem } from './TaskItem';
+
+
+export default function TaskList({tasks,onDelete}) {
+    
+    return (
+        <div>
+            {
+                tasks.map((task)=> (
+                   
+                    <TaskItem key={task.id} task = {task} onDelete={onDelete}/>
+                ))
+            }
+        </div>
+    )
+}
