16121a14b7cc9349cbb1edbd3e83c8b8b2f2b3ea
E-Ozdemir <emreclarusway@gmail.com>
2021-09-21 16:45:52 +0200

    createTask added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Header from "./components/Header";
 import {useState} from 'react'
 import { initialState } from './store/initialState';
 import TaskList from "./components/TaskList";
+import CreateTask from "./components/CreateTask";
 
 function App() {
   const [tasks, setTasks] = useState(initialState);
 
   //create task
+  
 
   //delete task
   const onDelete = (deletedItemID)=> setTasks(tasks.filter((task)=>task.id !== deletedItemID));
-
-
 
   //toggle done
 
@@ -21,6 +21,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker"/>
+      <CreateTask/>
       <TaskList tasks={tasks} onDelete={onDelete}/>
     </div>
   );
